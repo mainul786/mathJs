5df56cd194a020aff8987b5ea94ac323389f5d57
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-08-19 20:19:10 +0530

    Math pratice details

---
+++ math.js
@@ -0,0 +1,24 @@
+// pow();
+console.log(Math.pow(3, 2));
+
+// math.ceil
+const num = 4.908877;
+console.log(Math.ceil(num));
+
+// Math.floor()
+const num1 = 45.8756;
+console.log(Math.floor(num1));
+
+// Math.random();
+console.log(Math.random());
+
+// Math.round()
+const num2 = 234.657;
+const num3 = 3456.345;
+console.log(Math.round(num2));
+console.log(Math.round(num3));
+
+// Math.abs()
+const nmu4 = 7485892;
+const nmu5 = 7285892;
+console.log(Math.abs(nmu4 - nmu5));
